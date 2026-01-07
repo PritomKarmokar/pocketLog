3a01c17d3b10123cea4ae0930d1c7277bf33855f
Pritom Karmokar <pritomkarmokar99@gmail.com>
2026-01-08 01:46:49 +0600

    feat: adding customize user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from accounts.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+    list_display = ("username", "email", "is_staff", "is_active")
+    list_filter = ("username", "email")
+    ordering = ("email",)
+
+    search_fields = (
+        "email",
+        "username",
+    )
