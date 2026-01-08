bdc0399e2f7eb5e73071b9b2211ccbf922d32abe
Pritom Karmokar <pritomkarmokar99@gmail.com>
2026-01-08 14:59:52 +0600

    feat: customize admin part for user creation via admin portal

--- accounts/admin.py
+++ accounts/admin.py
@@ -6,6 +6,21 @@ from accounts.models import User
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
     model = User
+    # Fields shown when creating a user
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "username",
+                "email",
+                "password1",
+                "password2",
+                "is_staff",
+                "is_superuser",
+                "is_active",
+            ),
+        }),
+    )
 
     list_display = ("username", "email", "is_staff", "is_active")
     list_filter = ("username", "email")
